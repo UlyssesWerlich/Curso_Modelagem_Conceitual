73bb9e6ba108f3a22467a78dc95a2d8163f41617
Ulysses Werlich <werlichborgesulysses@gmail.com>
2021-03-21 12:05:21 -0500

    Criando repository e service para Categoria

---
+++ src/main/java/com/ulysseswerlich/cursomc/repositories/CategoriaRepository.java
@@ -0,0 +1,11 @@
+package com.ulysseswerlich.cursomc.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.ulysseswerlich.cursomc.domain.Categoria;
+
+@Repository
+public interface CategoriaRepository extends JpaRepository<Categoria, Integer>{
+
+}
---
+++ src/main/java/com/ulysseswerlich/cursomc/services/CategoriaService.java
@@ -0,0 +1,22 @@
+package com.ulysseswerlich.cursomc.services;
+
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.ulysseswerlich.cursomc.domain.Categoria;
+import com.ulysseswerlich.cursomc.repositories.CategoriaRepository;
+
+@Service
+public class CategoriaService {
+	
+	@Autowired
+	private CategoriaRepository repo;
+	
+	public Categoria buscar(Integer id) {
+		Optional<Categoria> obj = repo.findById(id);
+		return obj.orElse(null); 
+	}
+	
+}
