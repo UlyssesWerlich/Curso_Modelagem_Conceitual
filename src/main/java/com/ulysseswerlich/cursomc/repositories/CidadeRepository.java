34268b247ddaca53d0ddaabbcc1f6de980deb278
Ulysses Werlich <werlichborgesulysses@gmail.com>
2021-03-22 20:26:54 -0500

    Estado e Cidade

---
+++ src/main/java/com/ulysseswerlich/cursomc/repositories/CidadeRepository.java
@@ -0,0 +1,11 @@
+package com.ulysseswerlich.cursomc.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.ulysseswerlich.cursomc.domain.Cidade;
+
+@Repository
+public interface CidadeRepository extends JpaRepository<Cidade, Integer>{
+
+}
