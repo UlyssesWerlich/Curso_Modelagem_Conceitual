374cdb269bd8b15b63d4f3dc491d3f054299261a
Ulysses Werlich <werlichborgesulysses@gmail.com>
2021-04-09 12:11:26 -0500

    Validacao sintatica com Bean Validation

--- src/main/java/com/ulysseswerlich/cursomc/services/CategoriaService.java
+++ src/main/java/com/ulysseswerlich/cursomc/services/CategoriaService.java
@@ -11,6 +11,7 @@ import org.springframework.data.domain.Sort.Direction;
 import org.springframework.stereotype.Service;
 
 import com.ulysseswerlich.cursomc.domain.Categoria;
+import com.ulysseswerlich.cursomc.dto.CategoriaDTO;
 import com.ulysseswerlich.cursomc.repositories.CategoriaRepository;
 import com.ulysseswerlich.cursomc.services.exceptions.DataIntegrityException;
 import com.ulysseswerlich.cursomc.services.exceptions.ObjectNotFoundException;
@@ -53,6 +54,10 @@ public class CategoriaService {
 	public Page<Categoria> findPage(Integer page, Integer linesPerPage, String orderBy, String direction){
 		PageRequest pageRequest = PageRequest.of(page, linesPerPage, Direction.valueOf(direction), orderBy);
 		return repo.findAll(pageRequest);
+	}
+	
+	public Categoria fromDTO(CategoriaDTO objDTO) {
+		return new Categoria(objDTO.getId(), objDTO.getNome());
 	}
 	
 }
