a49e9a80e8f023fa3fbfb15710b19bd8df65efee
Ulysses Werlich <werlichborgesulysses@gmail.com>
2021-04-19 14:54:01 -0500

    Ajustando e testando a delecao de cliente

--- src/main/java/com/ulysseswerlich/cursomc/services/ClienteService.java
+++ src/main/java/com/ulysseswerlich/cursomc/services/ClienteService.java
@@ -57,7 +57,7 @@ public class ClienteService {
 		try {
 			repo.deleteById(id);
 		} catch (DataIntegrityViolationException e ){
-			throw new DataIntegrityException("Não é possível excluir o cliente");
+			throw new DataIntegrityException("Não é possível excluir o cliente porque há pedidos relacionados");
 		}
 	}
 
