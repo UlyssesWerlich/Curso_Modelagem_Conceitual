1449520a7925ffda77e4e869760ca33f7284c87e
Ulysses Werlich <werlichborgesulysses@gmail.com>
2021-04-28 13:38:02 -0500

    Inserindo Pedido

--- src/main/java/com/ulysseswerlich/cursomc/domain/PagamentoComCartao.java
+++ src/main/java/com/ulysseswerlich/cursomc/domain/PagamentoComCartao.java
@@ -2,9 +2,11 @@ package com.ulysseswerlich.cursomc.domain;
 
 import javax.persistence.Entity;
 
+import com.fasterxml.jackson.annotation.JsonTypeName;
 import com.ulysseswerlich.cursomc.domain.enums.EstadoPagamento;
 
 @Entity
+@JsonTypeName("pagamentoComCartao")
 public class PagamentoComCartao extends Pagamento {
 	private static final long serialVersionUID = 1L;
 
